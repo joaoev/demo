42a85221260f48af259d110a508970f8259688d6
joaoev <joaoalvesxp@alu.ufc.br>
2026-03-11 08:57:24 -0300

    feat: criando dto para listar produtos

---
+++ src/main/java/com/estudo/npi/demo/dto/ProdutoListarDto.java
@@ -0,0 +1,16 @@
+package com.estudo.npi.demo.dto;
+
+import java.util.Set;
+
+public record ProdutoListarDto(
+        Long id,
+        String nome,
+        String descricao,
+        Double preco,
+        Set<CategoriaDto> categorias
+) {
+    public record CategoriaDto(
+            Long id,
+            String nomeCategoria
+    ) {}
+}
